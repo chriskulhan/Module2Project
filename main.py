1bf2d927f29ae1628f2cabd4228df94576e2cbe4
chris_kulhanek <156239549+chriskulhan@users.noreply.github.com>
2025-01-22 18:59:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# North Loop Provisions - Donut Shop Management System
+# Your first Python program for Module 2
+
+def welcome_message():
+    """Display a welcome message for the donut shop."""
+    print("")
