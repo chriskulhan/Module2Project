d911b179ed939ca38d2ff80c5aa6fa279bed8637
chris_kulhanek <156239549+chriskulhan@users.noreply.github.com>
2025-01-22 19:37:03 +0000

    finished?

--- main.py
+++ main.py
@@ -33,3 +33,52 @@ if __name__ == "__main__" :
     #call the show menu message
     show_menu()
     print("\nReady to serve our community with the finest donuts!")
+
+    #Our complete menu organized by category
+    donut_menu = { #dictionary list
+        #list is the value for the key of Small Batch
+        'Small Batch': [
+            'Wild Rice & Honey',
+            'Maple Bacon',
+            'Swedish Cardomom'
+        ],
+        'Seasonal': [
+            'Apple Cider',
+            'Juicy Lucy',
+            'Lake of the Woods'
+        ], 
+        'Local Collabs': [
+            'Spyhouse Coffee Cake',
+            'Fulton Beer and Pretzel',
+            'Sweet Science Ice Cream'
+        ]
+    } #closing brace for the dictionary
+
+        #Locally-sourced toppings
+    toppings = [
+        'House-Made Sprinkles',
+        'Candied Hazelnuts',
+        'Bee Pollen',
+        'Cookie Butter Drizzle'
+    ]
+    
+    # Track our morning sales with a dictionary (because there will be key-value pairs)
+    morning_sales = []
+
+    #record our first sale
+    #Add a new item to a list by using .append to append a transaction to the sales dictionary
+    morning_sales.append ({
+        'item': 'Wild Rice & Honey',
+        'quantity': 2,
+        'toppings': ['Bee Pollen'],
+        'time': '7:30 AM'
+    })
+
+    #Display our current menu using a for loop
+    print("Today's Morning Menu:")
+    for category, items in donut_menu.items():
+        print(category + ":")
+        for item in items:
+            #this is a concatenation operator (using +) works 
+            print(" - " + item)
+
